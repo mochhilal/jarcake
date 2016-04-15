bc595112201bdbf0d8226c76efc9982e57ae22f7
HaeraniAdawiah <adawiahhaerani@gmail.com>
2016-04-15 18:46:09 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="anoceanofsky.css" />
+		<title>Jar Cake</title>
+	</head>
+	<body>
+	  <div id="page">
+	    <div class="topNaviagationLink"><a href="index.php">Beranda</a></div>
+			<div class="topNaviagationLink"><a href="jarcake.php">Jar Cake</a></div>
+			<div class="topNaviagationLink"><a href="pembelian.php">Pembelian</a></div>
+			<div class="topNaviagationLink"><a href="hubungi.php">Hubungi</a></div>
+		</div>
+		<div id="mainPicture">
+		  <div class="picture">
+		  
+		  </div>
+		</div>
+		<div class="contentBox">
+		  <div class="innerBox">
+		    <div class="contentTitle">Jar Cake</div>
+		    <div class="contentText"><p></p><br />
+		      <p>Selamat Datang Di Website Jar Cake</p><br />
+		      <p>
+		      <p>Hallo Guysss .....</p>
+		      <p>Disini kami akan sedikit memberikan informasi tentang Jar Cake </p>
+					<p>Pasti belum pada tau ya apa itu Jar Cake ? Jar Cake adalah sebuah cake yg dikemas cantik didalam sebuah jar/bottle..</p>
+					<p>Cake ini sudah terkenal diluar negeri. Rasanya pasti sangat menggoda dengan kelembutan cake dan cream yg sangat lezat 
+					dengan menggunakan bahan2 yg berkualitas membuat cita rasa cake ini sangat yummy.. </p>
+					<p>Bagi yg pada bingung ngasih kado ulgthn, souvenir atau hadiah anniversarry mau kasih sesuatu buat pacar, adik, mama 
+					atau siapapun cake ini sangat cocok loh :) </p>
+					<p>Cake ini juga kuat sampai 3 minggu dalam lemari es (tanpa dibuka) makan nya pas lagi dingin enak banget!</p>
+					<br></br>
+				</div>
+			</div>
+		</div>
+		<div id ="footer"><a href="http://www.aszx.net">web development</a> by <a href="http://bryantsmith.com">bryant smith</a></div>
+	</body>
+</html>
+		
