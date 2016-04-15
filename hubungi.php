6ba1b6679978607a8e3bc8ce30cedf747199e094
inayahnursupiyati <inayah@student.unsil.ac.id>
2016-04-15 18:50:50 +0700

    Create hubungi.php

---
+++ hubungi.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="anoceanofsky.css" />
+    <title>Jarcake</title>
+  </head>
+  
+<body>
+  <div id="page">
+    <div class="topNavigationLink"><a href="index.php">Beranda</a></div>
+    <div class="topNavigationLink"><a href="jarcake.php">Jar Cake</a></div>
+    <div class="topNavigationLink"><a href="pembelian.php">Pembelian</a></div>
+    <div class="topNavigationLink"><a href="hubingi.php">Hubungi</a></div>
+  </div>
+  <div id="mainPicture">
+    <div class="picture">
+    
+    </div>
+  </div>
+  <div class="contenBox">
+    <div class="innerBox">
+      <div class="contentTitle">Jar Cake</div>
+      <div class="contentTitle"><p></p><br />
+        <p>Untuk Pemesanan Bisa Hubungi</p>
+        <br />
+        <p>Pemilik      : I'nayah Nursupiyati</p>
+        <p>Kontak       : 082214693908</p>
+        <p>FB           : inayah nursupiyati</p>
+        <p>Untuk Pemesanan Wilayah Jawa Barat</p>
+        <br />
+        
+        <p>Pekerja      : Desi Rahmawati</p>
+        <p>Kontak       : 085221360886</p>
+        <p>FB           : desi rahmawati</p>
+        <p>Untuk pemesanan wilayah Jawa Tengah</p>
+        <br />
+        
+        <p>Pekerja      : Haerani Adawiah</p>
+        <p>Kontak       : 081320675432</p>
+        <p>FB           : rani adawiah kasyalayalia</p>
+        <p>Untuk pemesanan wilayah Jawa Timur</p>
+        <br />
+        
+        <p>Pekerjaan    : Muhammad Hilal</p>
+        <p>Kontak       : 089678543432</p>
+        <p>FB           : moch hilal</p>
+        <p>untuk pemesanan Jabodetabek</p>
+        <br />
+        
+        <p>Pekerjaan    : Aris Kurniawan</p>
+        <p>Kontak       : 0853224657897</p>
+        <p>FB           : aris kurniawan</p>
+        <p>Untuk pemesanan wilayah Luar Indonesia</p>
+        <br />
+      </div>
+    </div>
+  </div>
+  <div id="footer"><a href="http://www.aszx.net">web development</a> by 
+  <a href="http://www.bryantsmith.com">bryant smith</a></div>
+</body>
+</html>
+  
