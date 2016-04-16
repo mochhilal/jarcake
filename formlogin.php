09f8e0bb7fdeda9fefbfa29b0f5c0f2998bd0b1c
mochhilal <moch.hilal@gmail.com>
2016-04-16 18:15:14 +0700

    Update formlogin.php

--- formlogin.php
+++ formlogin.php
@@ -1,21 +1,19 @@
 <?php
-session_start();
-include('connect.php;);
-$user = $_POST ['username'];
-$pass = $_POST ['password'];
-$cekuser = mysql_query("SELECT *FROM datapembeli WHERE username = '$user'");
-$jumlah = mysql_num_rows($cekuser);
-$hasil = mysql_fetch_array($cekuser);
-if ($jumlah == 0 ) {
-  echo 'Username Belum Terdaftar!<br/>';
-  echo '<a href="login.php">&laquo; Back</a>';
-  } else {
-  if ( $pass <> $hasil['password'] ) {
-  echo 'Password Salah!<br/>';
-   echo '<a href="login.php">&laquo; Back</a>';
-    } else {
-        $_SESSION['username'] = $user;
-        header('location:pembelian.php');
-    }
-}
+	session_start();
+	require_once("connect.php");
+	$email = $_POST['email'];
+	$pass = $_POST['password'];
+	$query = $db->prepare("SELECT * FROM datapembeli WHERE email = ?");
+	$query->execute(array($email));
+	$hasil = $query->fetch();
+	if($query->rowCount() == 0) {
+		echo "<div align='center'>Email Belum Terdaftar! <a href='login.php'>Back</a></div>";
+	} else {
+		if($pass <> $hasil['password']) {
+			echo "<div align='center'>Password salah! <a href='login.php'>Back</a></div>";
+		} else {
+			$_SESSION['email'] = $hasil['email'];
+			header('location:pembelian.php');
+		}
+	}
 ?>
