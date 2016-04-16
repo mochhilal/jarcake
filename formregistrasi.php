b82354922a4a5081997f15bf184141c0fae70249
mochhilal <moch.hilal@gmail.com>
2016-04-16 18:16:43 +0700

    Update formregistrasi.php

--- formregistrasi.php
+++ formregistrasi.php
@@ -1,24 +1,24 @@
 <?php
-include ('connect.php');
-$nama = $_POST['nama'];
-$mail = $_POST['email'];
-$user = $_POST['username'];
-$pass = $_POST['password'];
-$cekuser= mysql_query("SELECT * FROM datapembeli WHERE username = '$user'");
-if (mysql_num_rows($cekuser) <> 0 ) {
-  echo 'Username Sudah Terdaftar! <br />
-  echo '<a href="login.php">$laquo; Back</a>';
-} else {
-  if ( !$nama || !$mail || !$user || !$pass ) {
-    echo 'Masih ada dta yang kosong!<br />';
-    echo '<a href="login.php">$laquo; Back</a>';
-  } else {
-    $simpan = mysql_query("INSERT INTO datapembeli VALUES ('','$nama','$mail','$user','$pass')");
-    if ($simpan) {
-      echo 'Pendaftaran Berhasil, Silahkan <a href="login.php">Login</a>';
-    } else {
-      echo 'Proses Gagal!';
-    }
-  }
-}
+	require_once("connect.php");
+	$nama = $_POST['nama'];
+	$email = $_POST['email'];
+	$username = $_POST['username'];
+	$pass = $_POST['password'];	
+	$query = $db->prepare("SELECT * FROM datapembeli WHERE email = ?");
+	$query->execute(array($email));	
+	if($query->rowCount() != 0) {
+		echo "<div align='center'>Email Sudah Terdaftar! <a href='login.php'>Back</a></div>";
+	} else {
+		if(!$nama || !$email || !$username || !$pass) {
+			echo "<div align='center'>Masih ada data yang kosong! <a href='login.php'>Back</a>";
+		} else {			
+			$sql = $db->prepare("INSERT INTO datapembeli (nama, email, username, password) VALUES (?, ?, ?, ?)");
+			$simpan = $sql->execute(array($nama, $email, $username, $pass));
+			if($simpan) {
+				echo "<div align='center'>Pendaftaran Sukses, Silahkan <a href='login.php'>Login</a></div>";
+			} else {
+				echo "<div align='center'>Proses Gagal!</div>";
+			}
+		}
+	}
 ?>
