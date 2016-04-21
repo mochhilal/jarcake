8578d212779d466aa16fa4498d45c1db324d6983
desirahmawati <desirahmawati@student.unsil.ac.id>
2016-04-21 22:13:39 +0700

    Create jarcake.php

---
+++ jarcake.php
@@ -0,0 +1,355 @@
+<?php
+  require_once("connect.php");
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Gallery<,/title>
+    <meta name="description" content="Jarcake Galery">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" href="anoceanofsky.css" />
+    <link href="css/style.min.css?06062013" rel="stylesheet">
+    <link href="css/prism.min.css" rel="stylesheet">
+    <style type="text/css">
+    </style>
+  </head>
+  <body>
+    <header>
+      <div id="page2">
+        <div class="topNavigationLink"><a href="index.php">Beranda</a></div>
+        <div class="topNavigationLink"><a href="jarcake.php">Jar Cake</a></div>
+        <div class="topNavigationLink"><a href="pembelian.php">Pembelian</a></div>
+      </div>
+    </header>
+    
+    <!-- Gallery -->
+    <section>
+      <ul id="gallery">
+        <li id="fullPreview"></li>
+        <li>
+          <a href="img/full/a.jpg"></a>
+          <img data-original="img/tumb/a.jpg" src="img/tumb/a.jpg" width="240" height="150" alt="01" style="display: inline;">
+          <div class="overLayer"></div>
+          <div class="infoLayer">
+            <ul>
+              <li>
+                <h2>
+                  Jar Cake
+                </h2>
+              </li>
+              <li>
+                <p>
+                  lihat gambar
+                </p>
+              </li>
+            </ul>
+          </div>
+          <p class="projectInfo"></p>
+        </li>
+        <li>
+          <a href="img/full/Oreo.jpg"></a>
+          <img data-original="img/tumb/Oreo.jpg" src="img/tumb/Oreo.jpg" width="240" height="150" alt="02" style="display: inline;">
+          
+          <div class="overLayer"></div>
+          <div class="infoLayer">
+            <ul>
+              <li>
+                <h2>
+                  Jar Cake
+                </h2>
+              </li>
+              <li>
+                <p>
+                  lihat gambar
+                </p>
+              </li>
+            </ul>
+          </div>
+          
+          <p class="projectInfo">
+            Jar Cake Rasa OREO -- IDR.35k
+          </p>
+        </li>
+        <li>
+          <a href="img/full/Fudge.jpg"></a>
+          <img data-original="img/tumb/Fudge.jpg" src="img/tumb/Fudge.jpg" width="240" height="150" alt="03" style="display: inline;">
+          
+          <div class="overLayer"></div>
+          <div class="infoLayer">
+            <ul>
+              <li>
+                <h2>
+                  Jar Cake
+                </h2>
+              </li>
+              <li>
+                <p>
+                  lihat gambar
+                </p>
+              </li>
+            </ul>
+          </div>
+          
+          <p class="projectInfo">
+            Jar Cake Rasa FUDGE -- IDR.35k
+          </p>
+        </li>
+        <li>
+          <a href="img/full/Chococheese.jpg"></a>
+          <img data-original="img/tumb/Chococheese.jpg" src="img/tumb/Chococheese.jpg" width="240" height="150" alt="04" style="display: inline;">
+          
+          <div class="overLayer"></div>
+          <div class="infoLayer">
+            <ul>
+              <li>
+                <h2>
+                  Jar Cake
+                </h2>
+              </li>
+              <li>
+                <p>
+                  lihat gambar
+                </p>
+              </li>
+            </ul>
+          </div>
+          
+          <p class="projectInfo">
+            Jar Cake Rasa CHOCOCHEESE -- IDR.35k
+          </p>
+        </li>
+        <li>
+          <a href="img/full/Greentea.jpg"></a>
+          <img data-original="img/tumb/Greentea.jpg" src="img/tumb/Greentea.jpg" width="240" height="150" alt="05" style="display: inline;">
+          
+          <div class="overLayer"></div>
+          <div class="infoLayer">
+            <ul>
+              <li>
+                <h2>
+                  Jar Cake
+                </h2>
+              </li>
+              <li>
+                <p>
+                  lihat gambar
+                </p>
+              </li>
+            </ul>
+          </div>
+          
+          <p class="projectInfo">
+            Jar Cake Rasa GREENTEA -- IDR.35k
+          </p>
+        </li>
+        <li>
+          <a href="img/full/Blueberry.jpg"></a>
+          <img data-original="img/tumb/Blueberry.jpg" src="img/tumb/Blueberry.jpg" width="240" height="150" alt="06" style="display: inline;">
+          
+          <div class="overLayer"></div>
+          <div class="infoLayer">
+            <ul>
+              <li>
+                <h2>
+                  Jar Cake
+                </h2>
+              </li>
+              <li>
+                <p>
+                  lihat gambar
+                </p>
+              </li>
+            </ul>
+          </div>
+          
+          <p class="projectInfo">
+            Jar Cake Rasa BLUEBERRY -- IDR.35k
+          </p>
+        </li>
+        <li>
+          <a href="img/full/Tiramisu.jpg"></a>
+          <img data-original="img/tumb/Tiramisu.jpg" src="img/tumb/Tiramisu.jpg" width="240" height="150" alt="07" style="display: inline;">
+          
+          <div class="overLayer"></div>
+          <div class="infoLayer">
+            <ul>
+              <li>
+                <h2>
+                  Jar Cake
+                </h2>
+              </li>
+              <li>
+                <p>
+                  lihat gambar
+                </p>
+              </li>
+            </ul>
+          </div>
+          
+          <p class="projectInfo">
+            Jar Cake Rasa TIRAMISU -- IDR.35k
+          </p>
+        </li>
+        <li>
+          <a href="img/full/Avomisu.jpg"></a>
+          <img data-original="img/tumb/Avomisu.jpg" src="img/tumb/Avomisu.jpg" width="240" height="150" alt="08" style="display: inline;">
+          
+          <div class="overLayer"></div>
+          <div class="infoLayer">
+            <ul>
+              <li>
+                <h2>
+                  Jar Cake
+                </h2>
+              </li>
+              <li>
+                <p>
+                  lihat gambar
+                </p>
+              </li>
+            </ul>
+          </div>
+          
+          <p class="projectInfo">
+            Jar Cake Rasa AVOMISU -- IDR.35k
+          </p>
+        </li>
+        <li>
+          <a href="img/full/Avocado.jpg"></a>
+          <img data-original="img/tumb/Avocado.jpg" src="img/tumb/Avocado.jpg" width="240" height="150" alt="09" style="display: inline;">
+          
+          <div class="overLayer"></div>
+          <div class="infoLayer">
+            <ul>
+              <li>
+                <h2>
+                  Jar Cake
+                </h2>
+              </li>
+              <li>
+                <p>
+                  lihat gambar
+                </p>
+              </li>
+            </ul>
+          </div>
+          
+          <p class="projectInfo">
+            Jar Cake Rasa AVOCADO -- IDR.35k
+          </p>
+        </li>
+        <li>
+          <a href="img/full/Strawberry.jpg"></a>
+          <img data-original="img/tumb/Strawberry.jpg" src="img/tumb/Strawberry.jpg" width="240" height="150" alt="10" style="display: inline;">
+          
+          <div class="overLayer"></div>
+          <div class="infoLayer">
+            <ul>
+              <li>
+                <h2>
+                  Jar Cake
+                </h2>
+              </li>
+              <li>
+                <p>
+                  lihat gambar
+                </p>
+              </li>
+            </ul>
+          </div>
+          
+          <p class="projectInfo">
+            Jar Cake Rasa STRAWBERRY -- IDR.35k
+          </p>
+        </li>
+        <li>
+          <a href="img/full/Banana.jpg"></a>
+          <img data-original="img/tumb/Banana.jpg" src="img/tumb/Banana.jpg" width="240" height="150" alt="11" style="display: inline;">
+          
+          <div class="overLayer"></div>
+          <div class="infoLayer">
+            <ul>
+              <li>
+                <h2>
+                  Jar Cake
+                </h2>
+              </li>
+              <li>
+                <p>
+                  lihat gambar
+                </p>
+              </li>
+            </ul>
+          </div>
+          
+          <p class="projectInfo">
+            Jar Cake Rasa BANANA -- IDR.35k
+          </p>
+        </li>
+        <li>
+          <a href="img/full/Redvelvet.jpg"></a>
+          <img data-original="img/tumb/Redvelvet.jpg" src="img/tumb/Redvelvet.jpg" width="240" height="150" alt="12" style="display: inline;">
+          
+          <div class="overLayer"></div>
+          <div class="infoLayer">
+            <ul>
+              <li>
+                <h2>
+                  Jar Cake
+                </h2>
+              </li>
+              <li>
+                <p>
+                  lihat gambar
+                </p>
+              </li>
+            </ul>
+          </div>
+          
+          <p class="projectInfo">
+            Jar Cake Rasa REDVELVET -- IDR.35k
+          </p>
+        </li>
+        <li>
+          <a href="img/full/Durian.jpg"></a>
+          <img data-original="img/tumb/Durian.jpg" src="img/tumb/Durian.jpg" width="240" height="150" alt="13" style="display: inline;">
+          
+          <div class="overLayer"></div>
+          <div class="infoLayer">
+            <ul>
+              <li>
+                <h2>
+                  Jar Cake
+                </h2>
+              </li>
+              <li>
+                <p>
+                  lihat gambar
+                </p>
+              </li>
+            </ul>
+          </div>
+          
+          <p class="projectInfo">
+            Jar Cake Rasa DURIAN -- IDR.35k
+          </p>
+        </li>
+        </ul>
+    </section>
+    <div id="footer"><a href="http://www.aszx.net">web development</a> by <a href="http://www.bryantsmith.com">bryant smith</a></div>
+    
+    
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery.lazyload.js"></script>
+    
+    <script src="js/least.min.js?06062013"></script>
+    <script src="js/prism.js"></script>
+    
+    <script>
+    $(document).ready(function(){
+      $('#gallery').least();
+    });
+    </script>
+</body></html>
